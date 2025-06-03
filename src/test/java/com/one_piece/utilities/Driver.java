c875e57900427ef1544d3e562a19901904d5618e
virchenkoann <virchenko.ann@gmail.com>
2025-06-03 17:52:20 -0400

    Update Driver.java

--- src/test/java/com/one_piece/utilities/Driver.java
+++ src/test/java/com/one_piece/utilities/Driver.java
@@ -2,4 +2,6 @@ package com.one_piece.utilities;
 
 public class Driver {
     //Some code that will return us Driver instance
+
+    //Make sure we will implement Driver Pool, for multi-threded testing
 }
