fc9649762ed9aa0b033cc5275bf2a24d0e2da7ac
virchenkoann <virchenko.ann@gmail.com>
2025-06-03 18:11:31 -0400

    Changes to Driver.java class

--- src/test/java/com/one_piece/utilities/Driver.java
+++ src/test/java/com/one_piece/utilities/Driver.java
@@ -3,5 +3,5 @@ package com.one_piece.utilities;
 public class Driver {
     //Some code that will return us Driver instance
 
-    //Make sure we will implement Driver Pool, for multi-threded testing
+    //Make sure we will implement Driver Pool, for multithreaded testing
 }
